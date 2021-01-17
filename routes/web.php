36a990cc1d83f3b62710deeaec297489b31ab5e5
Rosa Montserrat Cerro Ortiz <al221911582gmail.com>
2021-01-17 02:23:53 -0600

    Listado de Productos

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $productos = DB::table("Producto")->get();
+    return view('welcome',[ 
+        'productos' => $productos
+     ]);
 });
